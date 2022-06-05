fcdb9a751f12a6fd3a0c4d59cff30b9c73629f91
Oksana Vasyliuk <o.vasyiuk@gmail.com>
2022-06-05 12:02:33 +0200

    new file

---
+++ square.py
@@ -0,0 +1,8 @@
+text= input ("enter the side length:")
+num = int(text)
+print (num > 0)
+
+print ("The perimeter of a square with a side of a", num, \
+        "cm is", num*4, "cm.")
+print ("The area of a square with a side of", num, "cm is", \
+        num**2, "cm2.")
